fc834800e97ebe4f316ab694fac7fba5b2a58c30
Joao Galhardi <joaogalhardi.dev@gmail.com>
2021-09-12 01:11:02 -0300

    Criação de um readme detalhando o projeto

---
+++ javascript/script.js
@@ -0,0 +1,58 @@
+// Carne - 400 gr por pessoa + de 6 horas - 650
+// Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500 ml 
+
+// Crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("Calculando...");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+    resultado.innerHTML =  `<p>${Math.ceil (qtdTotalCarne / 1000)} Kg de Carne</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja/ 355)} Latas de Cerveja</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas/ 2000)} Garrafas 2l de Bebida</p>`;
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650;
+    }
+    else {
+        return 400;
+    }
+
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    }
+    else {
+        return 1200;
+    }
+
+}
+
+
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    }
+    else {
+        return 1000;
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,41 @@
+
+<h1 align="center">
+<br>
+Churrascômetro 
+<br>
+<br>
+
+</h1>
+
+<p align="center"> Projeto realizado que tem como objetivo calcular a quantidade de carne, bebida</p>
+
+<p align="center">
+  <a href="https://opensource.org/licenses/MIT">
+    <img src="https://img.shields.io/badge/License-MIT-blue.svg" alt="License MIT">
+  </a>
+</p>
+
+[//]: # (Add your gifs/images here:)
+<div>
+  <img src="IMAGE_1_URL" alt="demo" height="425">
+  <img src="IMAGE_2_URL" alt="demo" height="425">
+</div>
+
+<hr />
+
+## Features
+[//]: # (Add the features of your project here:)
+This app features all the latest tools and practices in mobile development!
+
+- 💹**HTML**  
+- 💹 **CSS** 
+- 💹 **JAVASCRIPT**
+
+## Getting started
+
+Describe here the way to use/install your project
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://opensource.org/licenses/MIT) page for details.
